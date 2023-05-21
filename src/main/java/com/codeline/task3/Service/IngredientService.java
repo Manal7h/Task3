b7a89ade0a5977d047047873ed7fe8928cd4c4c8
manal7h <manal7h.work@gmail.com>
2023-05-21 23:14:51 +0400

    create delete function in ingredient service

--- src/main/java/com/codeline/task3/Service/IngredientService.java
+++ src/main/java/com/codeline/task3/Service/IngredientService.java
@@ -1,9 +1,11 @@
 package com.codeline.task3.Service;
 import com.codeline.task3.Model.Ingredient;
+import com.codeline.task3.Model.Inventory;
 import com.codeline.task3.Model.Product;
 import com.codeline.task3.Repository.IngredientRepository;
 import com.codeline.task3.Repository.ProductRepository;
 import com.codeline.task3.Request.IngredientRequest;
+import com.codeline.task3.Request.InventoryRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Date;
@@ -25,6 +27,13 @@ public class IngredientService {
         ingredient.setCreatedDate(new Date());
         ingredientRepository.save(ingredient);
         return "Ingredient Created Successfully";
+    }
+
+    public void deleteIngredientById(IngredientRequest request) {
+        Ingredient ingredient = ingredientRepository.findById(request.getIngredientId()).get();
+        ingredient.setIsActive(false);
+        ingredient.setUpdatedDate(new Date());
+        ingredientRepository.save(ingredient);
     }
 
 }
