6e51bb6632673d72d93eb5680d6a2aaad788e5f3
manal7h <manal7h.work@gmail.com>
2023-05-22 08:49:45 +0400

    Create Email Details class in Model backage

--- src/main/java/com/codeline/task3/Mailing/Model/EmailDetails.java
+++ src/main/java/com/codeline/task3/Mailing/Model/EmailDetails.java
@@ -2,12 +2,17 @@ package com.codeline.task3.Mailing.Model;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmailDetails {
-
+    private List<String> recipient;
+    private String mgBody;
+    private String subject;
+    private String attachment;
 
 }
